6c6eab008bf60a385909762fa7ba3609acaf1749
Omar <74269367+3marDev@users.noreply.github.com>
2025-04-02 22:52:49 +0300

    Add files via upload

---
+++ site/athkar.js
@@ -0,0 +1,26 @@
+// nction openIndexA() {
+//   var x = document.getElementById("A");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+// function openIndexB() {
+//   var x = document.getElementById("B");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+// function openIndexC() {
+//   var x = document.getElementById("C");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
